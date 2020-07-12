9318125114070189c08b4833a12d3afc4c7fc3fc
MuhamedYoussef <mu7med.youssef@gmail.com>
2020-07-12 21:58:49 +0200

    did some changes

--- main/urls.py
+++ main/urls.py
@@ -4,5 +4,6 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='main/index.html')),
-    path('about', TemplateView.as_view(template_name='main/about.html'))
+    path('about', TemplateView.as_view(template_name='main/about.html')),
+    path('faq', TemplateView.as_view(template_name='main/faq.html')),
 ]
